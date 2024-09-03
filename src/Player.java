626a92e51d476f717a4a03919599c303e01c2be6
JeffreyWallace <jeffywipod@aol.com>
2024-09-03 16:02:33 -0700

    initial commit

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Player {
+    public HashMap<String,Integer> ability_Scores;
+    public HashMap<String,Integer> saving_Throws;
+    public HashMap<String,Integer> skill_Proficiencies;
+    public ArrayList<String> other_Proficiencies;
+    public ArrayList<String> language_proficiencies;
+    public int speed;
+    public String size;
+    public ArrayList<String> resistances;
+    public String abilityDescription;
+    public String race;
+    public Player(){
+
+    }
+}
---
+++ src/Race.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Race {
+    public ArrayList<String> abilityScoreImprovements;
+    public int speed;
+    public String size;
+    public HashMap<String,Integer> skill_Proficiencies;
+    public ArrayList<String> other_Proficiencies;
+    public ArrayList<String> language_proficiencies;
+    public ArrayList<String> resistances;
+    public String abilityDescription;
+    public String description;
+}

f11754d983c4dd63984bd9c6e5273493093e09d9
JeffreyWallace <jeffywipod@aol.com>
2024-09-03 17:16:53 -0700

    started on races

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Player {
     public HashMap<String,Integer> ability_Scores;
@@ -11,8 +12,50 @@ public class Player {
     public String size;
     public ArrayList<String> resistances;
     public String abilityDescription;
-    public String race;
+    public Race race;
+    public ArrayList<Race> avaliableRaces;
     public Player(){
+        //initalize all races
+    }
 
+    public int selectRace(String race) {
+        //verify race exists
+        Race currentRace = null;
+        for(Race r : avaliableRaces){
+            if(r.name.equals(race)){
+                currentRace = r;
+            }
+        }
+        if(currentRace==null){return 1;}
+        //set race
+        this.race = currentRace;
+        //set speed
+        speed=currentRace.speed;
+        //set size
+        size=currentRace.size;
+        //add resistances
+        resistances=currentRace.resistances;
+        //add asi by looping through all asi and adding them
+        for(String asi:ability_Scores.keySet()) {
+            currentRace.getASI(asi);
+        }
+
+        //add language proficiencies by looping through the races languages and calling the
+        //add language method that will add a
+        //language if the word is choose it prompts the user to input a language or
+        //select from a list of viable languages
+
+        return 0;
+    }
+    public void addLanguageProf(String lang){
+        if(lang.equalsIgnoreCase("choose")) {
+            System.out.println("Choose language proficiency");
+            String tempLang="";
+            Scanner input = new Scanner(System.in);
+            while(tempLang.equalsIgnoreCase("")) {
+                tempLang=input.nextLine();
+                //validate the language
+            }
+        }
     }
 }
--- src/Race.java
+++ src/Race.java
@@ -2,13 +2,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Race {
-    public ArrayList<String> abilityScoreImprovements;
+    public String name;
+    public HashMap<String,Integer> abilityScoreImprovements=new HashMap<>();
     public int speed;
     public String size;
-    public HashMap<String,Integer> skill_Proficiencies;
-    public ArrayList<String> other_Proficiencies;
-    public ArrayList<String> language_proficiencies;
-    public ArrayList<String> resistances;
-    public String abilityDescription;
-    public String description;
+    public HashMap<String,Integer> skill_Proficiencies=new HashMap<>();
+    public ArrayList<String> other_Proficiencies=new ArrayList<>();
+    public ArrayList<String> language_proficiencies=new ArrayList<>();
+    public ArrayList<String> resistances=new ArrayList<>();
+    public ArrayList<String> spells=new ArrayList<>();
+    public String abilityDescription="";
+    public int feat=0;
+    public String description="";
+    public Race() {
+        name="human";
+        speed=30;
+        size="Medium";
+        skill_Proficiencies.put("Choose",1);
+        language_proficiencies.add("Common");
+        language_proficiencies.add("Choose");
+        feat=1;
+        spells.add("lvl0 Light at level 5");
+    }
+
+    public Integer getASI(String asi){
+
+        if(abilityScoreImprovements.get(asi)!=null) {
+            return abilityScoreImprovements.get(asi);
+        }
+        return 0;
+    }
 }
